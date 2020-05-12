b3ff87bd7dbe4d241fd46d9b00534bf6e2c41d17
chen <715255995@qq.com>
2020-05-12 21:50:01 +0800

    工厂类修改

--- chen_core/src/main/java/com/chen/core/process/TestProcessFactory.java
+++ chen_core/src/main/java/com/chen/core/process/TestProcessFactory.java
@@ -13,28 +13,43 @@ import java.util.*;
 public class TestProcessFactory implements InitializingBean {
 
     @Resource
-    TsetProcessService[] testProcessService;
+    List<TsetProcessService> serviceList = new ArrayList<>();
 
     private static Map<String, TsetProcessService> serviceMap = new HashMap<String,TsetProcessService>();
 
-    private static List<TsetProcessService> serviceList = new ArrayList<>();
+    /**
+     * 继承InitializingBean接口会在类注入后立刻进行操作，把具体的工厂类放入对应的map中
+     * @throws Exception
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
-        if(testProcessService==null){
+        if(serviceList==null){
             log.info("testProcessService null");
         }
-        for(TsetProcessService service:testProcessService){
+        for(TsetProcessService service:serviceList){
             serviceMap.put(service.getType(),service);
-            serviceList.add(service);
         }
     }
+
+    /**
+     * 通过定义的方法名获得对应的执行类
+     * @param type
+     * @return
+     */
     public TsetProcessService getProcessService(String type){
         return serviceMap.get(type);
     }
+
+    /**
+     * 执行多个类的方法，通过二进制状态机判断是否执行
+     * @param num
+     * @return
+     */
     public List<TsetProcessService> getProcessServiceByInt(int num){
         List<TsetProcessService> returnList = new ArrayList<>();
         for (TsetProcessService service:serviceList) {
             int status = service.getNum();
+            //通过二进制状态进行控制
             if((status&num)==status){
                 returnList.add(service);
             }
