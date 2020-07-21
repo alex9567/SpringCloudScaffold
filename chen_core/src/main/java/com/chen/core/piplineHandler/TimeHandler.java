5d442568ee3a3d428af800ba5e8d1d98af8aca3f
chenhao <chenhao@qiekj.com>
2020-07-21 16:37:18 +0800

    添加一个责任链

---
+++ chen_core/src/main/java/com/chen/core/piplineHandler/TimeHandler.java
@@ -0,0 +1,40 @@
+package com.chen.core.piplineHandler;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(120)
+@Component
+@Slf4j
+public class TimeHandler implements Handler {
+    /**
+     * 处理接收到前端请求的逻辑
+     */
+    @Override
+    public void oneTask(HandlerContext ctx, Request request) {
+        log.info("次数检验,接受逻辑");
+        ctx.fireTaskOne(request);
+    }
+
+    /**
+     * 查询到task之后，进行task过滤的逻辑
+     */
+    @Override
+    public void twoTask(HandlerContext ctx, Task task) {
+        log.info("次数检验,过滤逻辑");
+        ctx.fireTaskTwo(task);
+    }
+
+    /**
+     * task过滤完成之后，处理执行task的逻辑
+     */
+    @Override
+    public void threeTask(HandlerContext ctx, Task task) {
+        log.info("次数检验,执行逻辑");
+        task.setNum(task.getNum()+1);
+        log.info(new Gson().toJson(task));
+        ctx.fireTaskThree(task);
+    }
+}
