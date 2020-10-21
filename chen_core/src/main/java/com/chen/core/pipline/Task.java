11ab33a3380168ea2ddcd47bbdfec84f46fa9849
chen <715255995@qq.com>
2020-10-21 23:22:51 +0800

    添加说明

--- chen_core/src/main/java/com/chen/core/pipline/Task.java
+++ chen_core/src/main/java/com/chen/core/pipline/Task.java
@@ -1,4 +1,19 @@
 package com.chen.core.pipline;
 
+/**
+ * 用于策略运行的context上下文
+ */
 public class Task {
+    /**
+     * 入参
+     */
+    private String params;
+    /**
+     * 中间参数
+     */
+    private String middle;
+    /**
+     * 结果
+     */
+    private String result;
 }
